c89f281d7e1c9eda39545e881148f06a8ac78d9e
Valerian Polizzi <valerian.polizzi@epitech.eu>
2017-08-13 16:25:26 +0200

    added index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
-const bodyParser = require('body-parser');
 
 // Get our API routes
-const api = require('./server/routes/api');
 
 const app = express();
 
